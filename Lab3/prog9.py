e4f9604962a6460388e26374eabc4bc1d28ba2cf
Deepthi-n1 <deepthinachari@gmail.com>
2025-02-05 15:45:40 +0530

    Create prog9.py

---
+++ Lab3/prog9.py
@@ -0,0 +1,13 @@
+def is_palindrome(string):
+    left, right = 0, len(string) - 1
+    
+    while right >= left:
+        if string[left].upper() != string[right].upper():
+            return False
+        left += 1
+        right -= 1
+    return True
+print(is_palindrome('malayalam')) 
+
+string = input("Enter a string: ")
+print(is_palindrome(string))
